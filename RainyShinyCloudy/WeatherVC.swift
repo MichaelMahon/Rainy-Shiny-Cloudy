55338012f046d43dcc85cbfd16d0e405764bbda2
Mike Mahon <michaelmahon92@verizon.net>
2016-12-31 17:36:19 -0500

    update ui with Alamofire response data

--- RainyShinyCloudy/WeatherVC.swift
+++ RainyShinyCloudy/WeatherVC.swift
@@ -17,7 +17,7 @@ class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var currentWeatherTypeLbl: UILabel!
     @IBOutlet weak var tableView: UITableView!
     
-    var currentWeather = CurrentWeather()
+    var currentWeather: CurrentWeather!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,11 +25,13 @@ class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         tableView.delegate = self
         tableView.dataSource = self
         
+        currentWeather = CurrentWeather()
         currentWeather.downloadWeatherDetails {
-            
+            self.updateMainUI()
         }
         
     }
+    
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
@@ -43,6 +45,15 @@ class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
         
         return cell
+    }
+    
+    func updateMainUI() {
+        print("IN UPDATEUI \(currentWeather.date) \(currentWeather.cityName) \(currentWeather.currentTemp) \(currentWeather.weatherType)")
+        dateLbl.text = currentWeather.date
+        currentTempLbl.text = "\(currentWeather.currentTemp)"
+        currentWeatherTypeLbl.text = currentWeather.weatherType
+        locationLbl.text = currentWeather.cityName
+        currentWeatherImg.image = UIImage(named: currentWeather.weatherType)
     }
 }
 
